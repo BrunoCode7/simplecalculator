d378dc08dcea1b6ec371c3dee5cf22e517440259
Baraa Hesham <ios@Baraas-Mac.local>
2018-09-22 12:51:29 -0700

    Initial commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,7 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var numberOne: UITextField!
+    @IBOutlet weak var numberTwo: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func buttonSum(_ sender: Any) {
+        let n1:Double = Double(numberOne.text!)!
+        let n2:Double = Double(numberTwo.text!)!
+        let num = n1 + n2
+        result.text="Result Sum = \(num)"
+    }
+    
+    @IBAction func buttonSubtract(_ sender: Any) {
+        let n1:Double = Double(numberOne.text!)!
+        let n2:Double = Double(numberTwo.text!)!
+        let num = n1 - n2
+        result.text="Result Subtraction = \(num)"    }
+    
+    @IBAction func buttonMultiply(_ sender: Any) {
+        let n1:Double = Double(numberOne.text!)!
+        let n2:Double = Double(numberTwo.text!)!
+        let num = n1 * n2
+        result.text="Result Multiplication = \(num)"
+        
+    }
+    
+    @IBAction func buttomDevision(_ sender: Any) {
+        let n1:Double = Double(numberOne.text!)!
+        let n2:Double = Double(numberTwo.text!)!
+        let num = n1 / n2
+        result.text="Result Devision = \(num)"
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
